67c7661758e8fea0c489b2d1c3581abf6c529f9b
revanthbalajiysd-max <revanthbalajiysd@gmail.com>
2026-04-30 23:51:19 +0530

    Time: 5 ms (57.69%), Space: 44.8 MB (66.12%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] dirs = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for(String dir : dirs){
+            if(dir.isEmpty() ||dir.equals(".")) continue;
+            if(dir.equals("..")){
+                if(!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(dir);
+            }
+        } 
+        return "/" + String.join("/",stack);
+    }
+}
